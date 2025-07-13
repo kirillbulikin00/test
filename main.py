0158977de0e8413bdbf449e5500622517f634817
lakustovdimka <lakustovdimka@gmail.com>
2025-07-14 02:48:55 +0500

    123

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def get_weather(latitude, longitude):
 # --- Использование скрипта ---
 
 if __name__ == "__main__":
-    # Координаты для greewedfddswfghwwqef, Казахстан (пример)
+    # Координаты для greewedfdswfghwwqef, Казахстан (пример)
     almaty_lat = 43.2567
     almaty_lon = 76.9286
 
